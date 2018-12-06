2346ad31fb06b8a3e3f9ca2e8b701deec105f374
Dustin Doan <dustin.ddoan@gmail.com>
2018-12-06 12:18:30 -0800

    login user

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const SALT_I = 10;
+require('dotenv').config();
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -59,8 +61,29 @@ userSchema.pre('save', function(next) {
   } else {
     next();
   }
- 
 })
+
+// check pasword
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+
+    cb(null, isMatch)
+  })
+}
+
+// generate token
+userSchema.methods.generateToken = function(cb) {
+  var user = this
+  var token = jwt.sign(user._id.toHexString(), process.env.SECRET)
+
+  user.token = token;
+  user.save(function(err, user) {
+    if (err)  return cb(err);
+
+    cb(null, user)
+  })
+}
 
 const User = mongoose.model('User', userSchema);
 
