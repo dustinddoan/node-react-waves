ee518ba6ce029d337e77404112e6849265a06971
Dustin Doan <dustin.ddoan@gmail.com>
2018-12-07 02:17:36 -0800

    auth route

--- server/server.js
+++ server/server.js
@@ -17,9 +17,39 @@ app.use(cookieParser())
 // Models
 const { User } = require('./models/User')
 
+// Middlewares
+const { auth } = require('./middleware/auth');
+
 // =========================
 //        USERS
 // =========================
+
+app.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true, 
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history
+
+  })
+})
+
+app.get('/api/user/logout', auth, (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.user._id},
+    { token: ''},
+    (err, doc) => {
+      if (err) return res.json({ success: false, err })
+
+      return res.status(200).send({ success: true })
+    }
+  )
+})
+
 app.post('/api/users/register', (req, res) => {
   const user  = new User(req.body);
 
@@ -28,7 +58,7 @@ app.post('/api/users/register', (req, res) => {
 
     res.status(200).json({
       success: true,
-      user: doc
+      // user: doc
     })
   })
 })
