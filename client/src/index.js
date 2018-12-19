1bbe6bfb2be71e195729a2d8a8955bf84f93ed76
Dustin Doan <dustin.ddoan@gmail.com>
2018-12-19 10:45:14 -0800

    setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Resources/css/styles.css'
-import Routes from './routes';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Resources/css/styles.css";
+
+import Routes from "./routes";
+import { BrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+import Reducers from './reducers'
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 ReactDOM.render(
-  <BrowserRouter>
-   <Routes />
-  </BrowserRouter> 
-  , document.getElementById('root'));
+  <Provider store={createStoreWithMiddleware(Reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
