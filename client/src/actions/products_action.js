7442f8840077855793e791b1c6df0f92c031e3ae
Dustin Doan <dustin.ddoan@gmail.com>
2019-01-29 10:18:33 -0800

    check box and radio option

--- client/src/actions/products_action.js
+++ client/src/actions/products_action.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { PRODUCT_SERVER } from '../Components/utils/misc';
-import {GET_PRODUCTS_BY_SELL, GET_PRODUCTS_BY_ARRIVAL } from './types';
+import {GET_PRODUCTS_BY_SELL, GET_PRODUCTS_BY_ARRIVAL, GET_BRANDS, GET_WOODS } from './types';
 
 export function getProductsBySell() {
   const res = axios.get(`${PRODUCT_SERVER}/articles?sortBy=sold&order=desc&limit=4`)
@@ -19,5 +19,25 @@ export function getProductsByArrival() {
   return {
     type: GET_PRODUCTS_BY_ARRIVAL,
     payload: res
+  }
+}
+
+export function getBrands() {
+  const request = axios.get(`${PRODUCT_SERVER}/brands`)
+    .then(res => res.data)
+
+  return {
+    type: GET_BRANDS,
+    payload: request
+  }
+}
+
+export function getWoods() {
+  const request = axios.get(`${PRODUCT_SERVER}/woods`)
+    .then(res => res.data)
+
+  return {
+    type: GET_WOODS,
+    payload: request
   }
 }
--- client/src/routes.js
+++ client/src/routes.js
@@ -6,6 +6,8 @@ import Auth from './HOC/auth.jsx'
 import Home from "./Components/Home";
 import RegisterLogin from './Components/Register_Login/index.jsx'
 import Register from './Components/Register_Login/register'
+import Shop from './Components/Shop'
+
 import UserDashboard from './Components/User'
 import Cart from './Components/Cart'
 
@@ -14,11 +16,12 @@ class Routes extends Component {
     return (
       <Layout>
         <Switch>
-          <Route path="/user/dashboard" exac component={Auth(UserDashboard, true)} />
-          <Route path="/user/cart" exac component={Auth(Cart, true)} />
-          <Route path="/register" exac component={Auth(Register, false)} />
-          <Route path="/register_login" exac component={Auth(RegisterLogin, false)} />
-          <Route path="/" exac component={Auth(Home, null)} />
+          <Route path="/user/dashboard" exact component={Auth(UserDashboard, true)} />
+          <Route path="/user/cart" exact component={Auth(Cart, true)} />
+          <Route path="/register" exact component={Auth(Register, false)} />
+          <Route path="/register_login" exact component={Auth(RegisterLogin, false)} />
+          <Route path="/shop" exact component={Auth(Shop, null)} />
+          <Route path="/" exact component={Auth(Home, null)} />
         </Switch>
       </Layout>
     );
